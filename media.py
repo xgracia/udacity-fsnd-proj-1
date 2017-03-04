e522595106d90b9a524f32bfa75eb04a02693627
Xavier <xgracia3@gmail.com>
2017-03-04 08:41:38 +0000

    Added a commet block and removed the show_trailer method

--- media.py
+++ media.py
@@ -1,11 +1,14 @@
-import webbrowser
-
 class Movie():
+    """Movie
+    
+    Attributes:
+        title       (str) : The title of the movie.
+        storyline   (str) : The storyline of the movie.
+        image_url   (str) : The URL of the movie's poster image.
+        trailer_url (str) : The URL of the movie's youtube trailer.
+    """
     def __init__(self, title, storyline, image_url, trailer_url):
         self.title = title
         self.storyline = storyline
         self.image_url = image_url
         self.trailer_url = trailer_url
-    
-    def show_trailer(self):
-        webbrowser.open(self.trailer_url)

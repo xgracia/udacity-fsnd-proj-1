862c90b2ac9a72d54977cc8e0c66728d44255437
Xavier <xgracia3@gmail.com>
2017-03-04 08:03:41 +0000

    Removed obsolete files. Main logic is now in server.py

--- entertainment_center.py
+++
@@ -1,8 +0,0 @@
-import media
-import fresh_tomatoes
-
-toy_story = media.Movie('Toy Story', 'Toys come to life', 'http://www.gstatic.com/tv/thumb/movieposters/17420/p17420_p_v8_ab.jpg', 'https://youtu.be/vwyZH85NQC4')
-avatar = media.Movie('Avatar', 'Pocahontas on an alien planet', 'http://img.csfd.cz/files/images/user/profile/159/129/159129345_9e5fe4.jpg', 'https://youtu.be/uZNHIU3uHT4')
-
-movies = [toy_story, avatar]
-fresh_tomatoes.open_movies_page(movies)

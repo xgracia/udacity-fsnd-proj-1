ebacf4943cdaaa97832b6e9c8ab43c148ec3e63a
Xavier <xgracia3@gmail.com>
2017-03-04 07:54:52 +0000

    Modified server.py to return a template instead of a static_file

--- server.py
+++ server.py
@@ -1,7 +1,14 @@
-import os
-from bottle import route, static_file
+from bottle import route, template
 
 @route('/')
 def index():
-    dirname = os.path.dirname(os.path.realpath(__file__))
-    return static_file('fresh_tomatoes.html', dirname)
+    movies = [{
+        'movie_title': 'Toy Story',
+        'poster_image_url': 'http://www.gstatic.com/tv/thumb/movieposters/17420/p17420_p_v8_ab.jpg',
+        'trailer_url': 'https://youtu.be/vwyZH85NQC4'
+    },{
+        'movie_title': 'Avatar',
+        'poster_image_url': 'http://img.csfd.cz/files/images/user/profile/159/129/159129345_9e5fe4.jpg',
+        'trailer_url': 'https://youtu.be/uZNHIU3uHT4'
+    }]
+    return template('fresh_tomatoes', movies=movies)
